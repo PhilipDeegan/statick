bdd9531c28a15b18dda13867961748e035b83c8c
dekken <philip.deegan@gmail.com>
2020-05-31 13:56:07 +0200

    tick from pip

--- res/ci/travis-osx.sh
+++ res/ci/travis-osx.sh
@@ -11,20 +11,12 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $CWD/../..
 ROOT=$PWD
 
-brew update
-brew install swig
-
 cd /System/Library/Frameworks/ImageIO.framework/Versions/A/Resources
 sudo ln -sf libJPEG.dylib /usr/local/lib/libJPEG.dylib
 sudo ln -sf libPng.dylib /usr/local/lib/libPng.dylib
 sudo ln -sf libTIFF.dylib /usr/local/lib/libTIFF.dylib
 sudo ln -sf libGIF.dylib /usr/local/lib/libGIF.dylib
 cd $ROOT
-
-R="https://github.com/X-DataInitiative/tick --depth 1 tick -b master --recursive"
-TM=(array base random base_model linear_model preprocessing robust prox solver)
-git clone $R && (cd tick && python3 -m pip install -r requirements.txt --user && \
-                 ./sh/mkn.sh ${TM[@]})
 
 KLOG=3 ./test.sh
 KLOG=3 mkn build test -tdOa -D_KUL_USE_MKL $XTRA \
